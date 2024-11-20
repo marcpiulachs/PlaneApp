b76f3f86efa5de8b2a863265486abcd8e6e6b318
Marc Piulachs <marc.piulachs@live.com>
2024-11-20 10:33:14 +0100

    Change Icon

--- android/app/src/main/kotlin/com/example/object_3d/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/object_3d/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.paperwings
+package com.paperwings.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:planeapp/main.dart';
+import 'package:paperwings/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
